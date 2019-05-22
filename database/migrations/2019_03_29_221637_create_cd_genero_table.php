6f174b8d612037e94c3296117031ae78d677f77b
gef12 <gerfersoncoelho@gmail.com>
2019-05-22 09:25:26 -0300

    tabelas responsaveis por conter info de que cd e artistas pertencem ao cd

--- database/migrations/2019_03_29_221637_create_cd_genero_table.php
+++ database/migrations/2019_03_29_221637_create_cd_genero_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
-
+//migrate da tabelas para unir genero e cds
 class CreateCdGeneroTable extends Migration
 {
     /**
